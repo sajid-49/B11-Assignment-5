d7b508a2753d0da698edafdeb5ae93075149e3af
sajid-49 <ahmedsajid.taradin07@gmail.com>
2025-03-09 00:49:54 +0600

    last one more update

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,10 @@ const btnImg = document.getElementById("btn-img");
 btnImg.addEventListener("click", function () {
   const rendColor = "#" + Math.floor(Math.random() * 12576443).toString(16);
   document.body.style.backgroundColor = rendColor;
-  console.log(rendColor);
+});
+
+document.getElementById("aside-btn").addEventListener("click", function () {
+  document.getElementById("aside-text").innerHTML = " ";
 });
 // btn-1
 document.getElementById("card-btn1").addEventListener("click", function () {
@@ -12,7 +15,6 @@ document.getElementById("card-btn1").addEventListener("click", function () {
   document.getElementById("text-number").innerText = sum;
 
   const mainNumber2 = getInnerTextById("main-number-2");
-  console.log(mainNumber2);
   const sum2 = mainNumber2 + 1;
   document.getElementById("main-number-2").innerText = sum2;
 
@@ -21,7 +23,6 @@ document.getElementById("card-btn1").addEventListener("click", function () {
   const node =
     document.getElementById("card-btn1").parentNode.parentNode.children[1]
       .innerText;
-  console.log(node);
 
   const asideText = document.getElementById("aside-text");
   const get = new Date().toLocaleTimeString();
@@ -42,11 +43,24 @@ document.getElementById("card-btn2").addEventListener("click", function () {
   document.getElementById("text-number").innerText = sum;
 
   const mainNumber2 = getInnerTextById("main-number-2");
-  console.log(mainNumber2);
   const sum2 = mainNumber2 + 1;
   document.getElementById("main-number-2").innerText = sum2;
 
   document.getElementById("card-btn2").setAttribute("disabled", true);
+
+  const node =
+    document.getElementById("card-btn2").parentNode.parentNode.children[1]
+      .innerText;
+
+  const asideText = document.getElementById("aside-text");
+  const get = new Date().toLocaleTimeString();
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <h1 class="mt-5 bg-[#F4F7FF] rounded-lg text-[#000000] p-2 text-sm">
+        You have Complete The Task ${node} at ${get}
+  </h1>
+  `;
+  asideText.appendChild(div);
 });
 
 // btn-3
@@ -57,11 +71,24 @@ document.getElementById("card-btn3").addEventListener("click", function () {
   document.getElementById("text-number").innerText = sum;
 
   const mainNumber2 = getInnerTextById("main-number-2");
-  console.log(mainNumber2);
   const sum2 = mainNumber2 + 1;
   document.getElementById("main-number-2").innerText = sum2;
 
   document.getElementById("card-btn3").setAttribute("disabled", true);
+
+  const node =
+    document.getElementById("card-btn3").parentNode.parentNode.children[1]
+      .innerText;
+
+  const asideText = document.getElementById("aside-text");
+  const get = new Date().toLocaleTimeString();
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <h1 class="mt-5 bg-[#F4F7FF] rounded-lg text-[#000000] p-2 text-sm">
+        You have Complete The Task ${node} at ${get}
+  </h1>
+  `;
+  asideText.appendChild(div);
 });
 
 // btn-4
@@ -72,11 +99,24 @@ document.getElementById("card-btn4").addEventListener("click", function () {
   document.getElementById("text-number").innerText = sum;
 
   const mainNumber2 = getInnerTextById("main-number-2");
-  console.log(mainNumber2);
   const sum2 = mainNumber2 + 1;
   document.getElementById("main-number-2").innerText = sum2;
 
   document.getElementById("card-btn4").setAttribute("disabled", true);
+
+  const node =
+    document.getElementById("card-btn4").parentNode.parentNode.children[1]
+      .innerText;
+
+  const asideText = document.getElementById("aside-text");
+  const get = new Date().toLocaleTimeString();
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <h1 class="mt-5 bg-[#F4F7FF] rounded-lg text-[#000000] p-2 text-sm">
+        You have Complete The Task ${node} at ${get}
+  </h1>
+  `;
+  asideText.appendChild(div);
 });
 
 // btn-5
@@ -87,11 +127,24 @@ document.getElementById("card-btn5").addEventListener("click", function () {
   document.getElementById("text-number").innerText = sum;
 
   const mainNumber2 = getInnerTextById("main-number-2");
-  console.log(mainNumber2);
   const sum2 = mainNumber2 + 1;
   document.getElementById("main-number-2").innerText = sum2;
 
   document.getElementById("card-btn5").setAttribute("disabled", true);
+
+  const node =
+    document.getElementById("card-btn5").parentNode.parentNode.children[1]
+      .innerText;
+
+  const asideText = document.getElementById("aside-text");
+  const get = new Date().toLocaleTimeString();
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <h1 class="mt-5 bg-[#F4F7FF] rounded-lg text-[#000000] p-2 text-sm">
+        You have Complete The Task ${node} at ${get}
+  </h1>
+  `;
+  asideText.appendChild(div);
 });
 
 // btn-6
@@ -102,9 +155,22 @@ document.getElementById("card-btn6").addEventListener("click", function () {
   document.getElementById("text-number").innerText = sum;
 
   const mainNumber2 = getInnerTextById("main-number-2");
-  console.log(mainNumber2);
   const sum2 = mainNumber2 + 1;
   document.getElementById("main-number-2").innerText = sum2;
 
   document.getElementById("card-btn6").setAttribute("disabled", true);
+
+  const node =
+    document.getElementById("card-btn6").parentNode.parentNode.children[1]
+      .innerText;
+
+  const asideText = document.getElementById("aside-text");
+  const get = new Date().toLocaleTimeString();
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <h1 class="mt-5 bg-[#F4F7FF] rounded-lg text-[#000000] p-2 text-sm">
+        You have Complete The Task ${node} at ${get}
+  </h1>
+  `;
+  asideText.appendChild(div);
 });
--- scripts/utility.js
+++ scripts/utility.js
@@ -3,3 +3,18 @@ function getInnerTextById(id) {
   const convertedValue = parseInt(value);
   return convertedValue;
 }
+
+const liveDate = document.getElementById("live-date");
+
+const setTime = new Date().toLocaleDateString("en-BD", {
+  month: "short",
+  day: "numeric",
+  year: "numeric",
+});
+
+document.getElementById("full-date").innerText = setTime;
+
+const WeekName = new Date().toLocaleDateString("en-BD", {
+  weekday: "short",
+});
+liveDate.innerText = WeekName;
