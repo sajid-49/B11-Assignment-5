b9898510bf29d8636b2665f24276e3937ea92abc
sajid-49 <ahmedsajid.taradin07@gmail.com>
2025-03-08 20:25:59 +0600

    some more update

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
+const btnImg = document.getElementById("btn-img");
+btnImg.addEventListener("click", function () {
+  const rendColor = "#" + Math.floor(Math.random() * 12576443).toString(16);
+  document.body.style.backgroundColor = rendColor;
+  console.log(rendColor);
+});
 // btn-1
 document.getElementById("card-btn1").addEventListener("click", function () {
   alert("Board updated successfully.");
@@ -11,6 +17,21 @@ document.getElementById("card-btn1").addEventListener("click", function () {
   document.getElementById("main-number-2").innerText = sum2;
 
   document.getElementById("card-btn1").setAttribute("disabled", true);
+
+  const node =
+    document.getElementById("card-btn1").parentNode.parentNode.children[1]
+      .innerText;
+  console.log(node);
+
+  const asideText = document.getElementById("aside-text");
+  const get = new Date().toLocaleTimeString();
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <h1 class="mt-5 bg-[#F4F7FF] rounded-lg text-[#000000] p-2 text-sm">
+        You have Complete The Task ${node} at ${get}
+  </h1>
+  `;
+  asideText.appendChild(div);
 });
 
 // btn-2
